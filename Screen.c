b572c41ca59f9d2a3a319e181a436c6967fa60cb
Cornucopia27 <ie703121@iteso.mx>
2017-10-25 10:20:49 -0500

    Screen comments and screen changes

--- Screen.c
+++ Screen.c
@@ -15,15 +15,17 @@
 
 void float_String(ufloat32 fl)
 {
-	uint16 entero = (int)fl;
-	ufloat32 flotante = (fl-entero)*100;
-	uint16 helper = (int)flotante;
+	uint16 entero = (int)fl;	//makes cast to get the integer part of the float given
+	ufloat32 flotante = (fl-entero)*100; //multiplies by 100 the float decimal value
+	uint16 helper = (int)flotante; // makes cast to get get the integer part of the float decimals
+	/*uint values created to hold each character of the number*/
 	uint16 centenas = 0;
 	uint16 decenas = 0;
 	uint16 unidades = 0;
 	uint16 decimas = 0;
 	uint16 centesimas = 0;
 
+	/*while eliminating the biggest value of the whole number and the decimal part of the float it stores the value in the variables created*/
 	centenas = (uint16) entero/100;
 	entero = entero-(centenas*100);
 	decenas = (uint16) entero/10;
@@ -33,12 +35,14 @@ void float_String(ufloat32 fl)
 	helper = helper-(decimas*10);
 	centesimas = helper;
 
+	/*ascii convertion of the numbers*/
 	centenas+=48;
 	decenas+=48;
 	unidades+=48;
 	decimas+=48;
 	centesimas+=48;
 
+	/*saves the value of the numbers on the conversion array*/
 	conversion[0] = centenas;
 	conversion[1] = decenas;
 	conversion[2] = unidades;
@@ -50,6 +54,7 @@ void float_String(ufloat32 fl)
 void Screen_Config(ProgrmaState state)
 {
 
+	/*strings that hold the strings that will be inyected into the screen*/
 	uint8 multi_perc[] = "%";
 	uint8 multi_Cel[] = "'C";
 	uint8 multi_Change[] = "(-)B1 (+)B2   (ok)B3";
@@ -74,13 +79,14 @@ void Screen_Config(ProgrmaState state)
 	uint8 sub5_Freq[] = "Frecuencia";
 	uint8 sub5_Hz[] = "(Hz)";
 
+	/*switch with the current state of our systemstatus*/
 	switch(state)
 	{
 	case MAIN_STATE:
-		float_String(getSystemStatus()->motor.velocityValue);
-        LCDNokia_clear();
-		LCDNokia_gotoXY(10,0);
-		LCDNokia_sendString(main_Vel);
+		float_String(getSystemStatus()->motor.velocityValue); //converts current motor value into a string
+        LCDNokia_clear(); //clears the screen
+		LCDNokia_gotoXY(10,0); //goes to a specific position where we're gonna put the string
+		LCDNokia_sendString(main_Vel); // introduces the string
 		delay(65000);
 		LCDNokia_gotoXY(25,1);
 		LCDNokia_sendString(conversion);
@@ -91,7 +97,7 @@ void Screen_Config(ProgrmaState state)
 		LCDNokia_gotoXY(7,2);
 		LCDNokia_sendString(main_Temp);
 		delay(65000);
-		float_String(getSystemStatus()->temperature.celsiusValue);
+		float_String(getSystemStatus()->temperature.celsiusValue);// converts current celsius value into a string
 		LCDNokia_gotoXY(25,3);
 		LCDNokia_sendString(conversion);
 		delay(65000);
@@ -118,33 +124,43 @@ void Screen_Config(ProgrmaState state)
 		delay(65000);
 	break;
 	case ALARMA_STATE:
-        LCDNokia_clear();
-		LCDNokia_gotoXY(20,1);
-		LCDNokia_sendString(sub1_Al);
-		delay(65000);
-		if((B2 | B1) == getSystemStatus()->pressedButton)
+		if((B2 | B1) == getSystemStatus()->pressedButton) // if either the b1 or the b2 were pressed shows the value the alarm is gonna be switched to instead of its current value
 		{
-			float_String(getSystemStatus()->alarm.alarmMonitor);
+	        LCDNokia_clear();
+			LCDNokia_gotoXY(20,1);
+			LCDNokia_sendString(sub1_Al);
+			delay(65000);
+			float_String(getSystemStatus()->alarm.alarmMonitor); // converts the value the alarm is gonna be switched to into a string
 			LCDNokia_gotoXY(25,2);
 			LCDNokia_sendString(conversion);
+			delay(65000);
+			LCDNokia_gotoXY(40,2);
+			LCDNokia_sendString(multi_Cel);
+			delay(65000);
+			LCDNokia_gotoXY(0,3);
+			LCDNokia_sendString(multi_Change);
 			delay(65000);
 		}else{
-			float_String(getSystemStatus()->alarm.alarmaValue);
+	        LCDNokia_clear();
+			LCDNokia_gotoXY(20,1);
+			LCDNokia_sendString(sub1_Al);
+			delay(65000);
+			float_String(getSystemStatus()->alarm.alarmaValue); // converts the current alarm value into a string
 			LCDNokia_gotoXY(25,2);
 			LCDNokia_sendString(conversion);
 			delay(65000);
+			LCDNokia_gotoXY(40,2);
+			LCDNokia_sendString(multi_Cel);
+			delay(65000);
+			LCDNokia_gotoXY(0,3);
+			LCDNokia_sendString(multi_Change);
+			delay(65000);
 		}
-		LCDNokia_gotoXY(40,2);
-		LCDNokia_sendString(multi_Cel);
-		delay(65000);
-		LCDNokia_gotoXY(0,3);
-		LCDNokia_sendString(multi_Change);
-		delay(65000);
 	break;
 	case FORMAT_STATE:
-		if(CELSIUS == getSystemStatus()->temperature.typeDeegrees)
+		if(CELSIUS == getSystemStatus()->temperature.typeDeegrees) // if the current system value is celcius shows the current celcius value and its sign
 		{
-			float_String(getSystemStatus()->temperature.celsiusValue);
+			float_String(getSystemStatus()->temperature.celsiusValue); // converts the current celcius value into a string
 			LCDNokia_clear();
 			LCDNokia_gotoXY(0,1);
 			LCDNokia_sendString(sub2_For_Temp);
@@ -162,9 +178,9 @@ void Screen_Config(ProgrmaState state)
 			LCDNokia_sendString(sub2_Change);
 			delay(65000);
 		}
-		else if(FAHRENHEIT == getSystemStatus()->temperature.typeDeegrees)
+		else if(FAHRENHEIT == getSystemStatus()->temperature.typeDeegrees) // if the current system value is fahrenheit shows the current fahrenheit value and its sign
 		{
-			float_String(getSystemStatus()->temperature.fahrenheitValue);
+			float_String(getSystemStatus()->temperature.fahrenheitValue); // converts the current fahrenheit value into a string
 			LCDNokia_clear();
 			LCDNokia_gotoXY(0,1);
 			LCDNokia_sendString(sub2_For_Temp);
@@ -184,9 +200,9 @@ void Screen_Config(ProgrmaState state)
 		}
 	break;
 	case CHANGE_STATE:
-		if(B1 == getSystemStatus()->pressedButton)
+		if(B1 == getSystemStatus()->pressedButton) // checks if we're decreasing to show we're decreasing the treshold and shows the value it's gonna be switched to
 		{
-			float_String(getSystemStatus()->alarm.decrementMonitor);
+			float_String(getSystemStatus()->alarm.decrementMonitor); // converts the value its gonna be switched to into a string
 			LCDNokia_clear();
 			LCDNokia_gotoXY(10,1);
 			LCDNokia_sendString(sub3_Decre);
@@ -194,8 +210,14 @@ void Screen_Config(ProgrmaState state)
 			LCDNokia_gotoXY(25,2);
 			LCDNokia_sendString(conversion);
 			delay(65000);
-		}else if(B2 == getSystemStatus()->pressedButton){
-			float_String(getSystemStatus()->alarm.decrementMonitor);
+			LCDNokia_gotoXY(45,2);
+			LCDNokia_sendString(multi_perc);
+			delay(65000);
+			LCDNokia_gotoXY(0,3);
+			LCDNokia_sendString(multi_Change);
+			delay(65000);
+		}else if(B2 == getSystemStatus()->pressedButton){ //checks if we're increasing to show we're increasing the treshold and shows the value it's gonna be switched to
+			float_String(getSystemStatus()->alarm.decrementMonitor); // converts the value its gonna be switched to into a string
 			LCDNokia_clear();
 			LCDNokia_gotoXY(10,1);
 			LCDNokia_sendString(sub3_Inc);
@@ -203,8 +225,14 @@ void Screen_Config(ProgrmaState state)
 			LCDNokia_gotoXY(25,2);
 			LCDNokia_sendString(conversion);
 			delay(65000);
-		}else{
-			float_String(getSystemStatus()->alarm.decrementValue);
+			LCDNokia_gotoXY(45,2);
+			LCDNokia_sendString(multi_perc);
+			delay(65000);
+			LCDNokia_gotoXY(0,3);
+			LCDNokia_sendString(multi_Change);
+			delay(65000);
+		}else{ 													//if we haven't pressed any buttons shows the current value of the treshold
+			float_String(getSystemStatus()->alarm.decrementValue); //converts the current value of the treshold into a string
 			LCDNokia_clear();
 			LCDNokia_gotoXY(10,1);
 			LCDNokia_sendString(sub3_Decre);
@@ -212,43 +240,56 @@ void Screen_Config(ProgrmaState state)
 			LCDNokia_gotoXY(25,2);
 			LCDNokia_sendString(conversion);
 			delay(65000);
-		}
 			LCDNokia_gotoXY(45,2);
 			LCDNokia_sendString(multi_perc);
 			delay(65000);
 			LCDNokia_gotoXY(0,3);
 			LCDNokia_sendString(multi_Change);
 			delay(65000);
+		}
 	break;
 	case MANUAL_STATE:
-			LCDNokia_clear();
-			LCDNokia_gotoXY(5,0);
-			LCDNokia_sendString(sub4_Ctrl);
-			delay(65000);
-			if((B4|B5) == getSystemStatus()->pressedButton)
+			if((B4|B5) == getSystemStatus()->pressedButton) // if either B4 or B5 we're pressed shows the value the motor is gonna be switched to
 			{
-			float_String(getSystemStatus()->motor.velocityMonitor);
-			LCDNokia_gotoXY(30,1);
-			LCDNokia_sendString(conversion);
-			delay(65000);
-			}else{
-				float_String(getSystemStatus()->motor.velocityValue);
+				LCDNokia_clear();
+				LCDNokia_gotoXY(5,0);
+				LCDNokia_sendString(sub4_Ctrl);
+				delay(65000);
+				float_String(getSystemStatus()->motor.velocityMonitor); // converts value the motor is gonna be switched to into a string
 				LCDNokia_gotoXY(30,1);
 				LCDNokia_sendString(conversion);
 				delay(65000);
+				LCDNokia_gotoXY(50,1);
+				LCDNokia_sendString(multi_perc);
+				delay(65000);
+				LCDNokia_gotoXY(0,2);
+				LCDNokia_sendString(sub4_Change);
+				delay(65000);
+				LCDNokia_gotoXY(5,5);
+				LCDNokia_sendString(sub4_Change2);
+				delay(65000);
+			}else{	// if we havent pressed anything shows the current motor value
+				LCDNokia_clear();
+				LCDNokia_gotoXY(5,0);
+				LCDNokia_sendString(sub4_Ctrl);
+				delay(65000);
+				float_String(getSystemStatus()->motor.velocityValue);  // converts current motor value into string
+				LCDNokia_gotoXY(30,1);
+				LCDNokia_sendString(conversion);
+				delay(65000);
+				LCDNokia_gotoXY(50,1);
+				LCDNokia_sendString(multi_perc);
+				delay(65000);
+				LCDNokia_gotoXY(0,2);
+				LCDNokia_sendString(sub4_Change);
+				delay(65000);
+				LCDNokia_gotoXY(5,5);
+				LCDNokia_sendString(sub4_Change2);
+				delay(65000);
 			}
-		LCDNokia_gotoXY(50,1);
-		LCDNokia_sendString(multi_perc);
-		delay(65000);
-		LCDNokia_gotoXY(0,2);
-		LCDNokia_sendString(sub4_Change);
-		delay(65000);
-		LCDNokia_gotoXY(5,5);
-		LCDNokia_sendString(sub4_Change2);
-		delay(65000);
 	break;
 	case FRECUENCY_STATE:
-		float_String(getSystemStatus()->frecuency);
+		float_String(getSystemStatus()->frecuency); // converts the frequency value into a string
 		LCDNokia_clear();
 		LCDNokia_gotoXY(10,1);
 		LCDNokia_sendString(sub5_Freq);
--- Screen.h
+++ Screen.h
@@ -8,9 +8,26 @@
 #ifndef SCREEN_H_
 #define SCREEN_H_
 
-static uint8 conversion[6];
+static uint8 conversion[6]; // string value we're gonna inyect into the screen when transforming from float to string
 
+/********************************************************************************************/
+/********************************************************************************************/
+/********************************************************************************************/
+/*!
+ 	 \brief	This function changes a float variable into a string by decomposing the number and turning them to integers then converting them into their
+ 	 	 	 ascii equivalents and saving them into a string array
+ 	 \param[in]  float value thats gonna be converted into a string
+ 	 \return void
+ */
 void float_String(ufloat32 fl);
+/********************************************************************************************/
+/********************************************************************************************/
+/********************************************************************************************/
+/*!
+ 	 \brief	This function updates the current strings shown in the nokia screen and switches between set menus
+ 	 \param[in]  current systemstatus state
+ 	 \return void
+ */
 void Screen_Config(ProgrmaState state);
 
 
