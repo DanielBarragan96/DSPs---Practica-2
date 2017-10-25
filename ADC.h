f25e6dd48504aeaa03b28e5758e6281b87c9dafe
Cornucopia27 <ie703121@iteso.mx>
2017-10-25 11:41:33 -0500

    fixed comments on ADC and Screen

--- ADC.h
+++ ADC.h
@@ -38,14 +38,33 @@ typedef enum{
 	ADC_16Bits
 }ADC_CFG1_MODE;
 
+//to select the clk
 typedef enum{
 	BUS_CLOCK,
 	AT_CLOCK_2,
 	AT_CLOCK,
 	AS_CLOCK
 }ADC_CFG1_ADICLK;
-
+/********************************************************************************************/
+/********************************************************************************************/
+/********************************************************************************************/
+/*!
+ 	 \brief	Parametric select of the adc configuration of the SC1 and setup of the high sample rate and a hardware sampling enable and mode
+ 	 \param[in]  power selects between normal and low power mode.
+ 	 \param[in]  div selects the value the clk will be divided by.
+ 	 \param[in]  sample selects between a short or long sample time.
+ 	 \param[in]  mode selects between the different adc bitmodes ranges from 8 to 16.
+ 	 \param[in]  clk selects which clk will be used
+ 	 \return void
+ */
 void ADC_init(ADC_CFG1_DLPC power, ADC_CFG1_ADIV div, ADC_CFG1_ADLSMP sample, ADC_CFG1_MODE mode, ADC_CFG1_ADICLK clk);
+/********************************************************************************************/
+/********************************************************************************************/
+/********************************************************************************************/
+/*!
+ 	 \brief	 This function converts values the adc is reading and saves it on a 16 bit variable when the transmision stops.
+ 	 \return the values the adc read.
+ */
 uint16 ADC_Values();
 ufloat32 setMeasuredValue();
 
--- Screen.h
+++ Screen.h
@@ -8,7 +8,24 @@
 #ifndef SCREEN_H_
 #define SCREEN_H_
 
+/********************************************************************************************/
+/********************************************************************************************/
+/********************************************************************************************/
+/*!
+ 	 \brief	This function changes a float variable into a string by decomposing the number and turning them to integers then converting them into their
+ 	 	 	 ascii equivalents and saving them into a string array
+ 	 \param[in]  float value thats gonna be converted into a string
+ 	 \return void
+ */
 void float_String(ufloat32 fl);
+/********************************************************************************************/
+/********************************************************************************************/
+/********************************************************************************************/
+/*!
+ 	 \brief	This function updates the current strings shown in the nokia screen and switches between set menus
+ 	 \param[in]  current systemstatus state
+ 	 \return void
+ */
 void Screen_Config(ProgrmaState state);
 
 
